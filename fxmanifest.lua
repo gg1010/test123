76c5659f0e208dba434232b2000833fc052e169d
bb <55393468+gg1010@users.noreply.github.com>
2023-04-07 00:39:36 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,30 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+version '1.0.2'
+
+lua54 'yes'
+
+client_script {
+    'core.lua',
+    'client/main.lua',
+}
+server_script {
+    '@oxmysql/lib/MySQL.lua',
+    'core.lua',
+    'server/config.lua',
+    'server/functions.lua',
+    'server/main.lua'
+}
+
+ui_page "nui/index.html"
+files { 'nui/**/*' }
+
+escrow_ignore {
+    'core.lua',
+    'server/config.lua',
+    'server/functions.lua',
+    'client/**',
+    'nui/**',
+}
+dependency '/assetpacks'
